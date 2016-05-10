e9daefa5d217fc88a16a28da64acb80055f29bfb
Cameron Eckelberry <cameck@gmail.com>
2016-05-10 10:40:09 -0400

    Changed some formatting

--- voter_sim.rb
+++ voter_sim.rb
@@ -1,5 +1,5 @@
-#This is a REPL directory creator for a mock Voting database.
-#It allows you to use all aspects of CRUD.
+# This is a REPL directory creator for a mock Voting database.
+# It allows you to use all aspects of CRUD.
 require './classes/world'
 
 # start program
